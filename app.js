b16dfb108f325d05f08645d5c245bd2e731e7e63
shub1711 <srivastav.golu.ss@gmail.com>
2022-09-02 23:10:33 +0530

    added heroku port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { json } = require("express");
 
 const app = express();
 app.use(express.json());
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGODB_URI)
